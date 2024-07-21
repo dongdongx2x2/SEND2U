a571451ba6de041c3e3fd7d006b379128be3fa6d
donghyun <kdhdh001@naver.com>
2024-07-21 19:52:47 +0900

    🔨refactor: MessageRepository findAllWithUsers 추가

--- backend/src/main/java/com/ssafy/send2u/message/repository/MessageRepository.java
+++ backend/src/main/java/com/ssafy/send2u/message/repository/MessageRepository.java
@@ -4,7 +4,11 @@ import com.ssafy.send2u.message.entity.Message;
 import com.ssafy.send2u.user.entity.user.User;
 import java.util.List;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 
 public interface MessageRepository extends JpaRepository<Message, Long> {
     List<Message> findByReceiver(User receiver);
+
+    @Query("SELECT m FROM Message m JOIN FETCH m.sender JOIN FETCH m.receiver")
+    List<Message> findAllWithUsers();
 }
